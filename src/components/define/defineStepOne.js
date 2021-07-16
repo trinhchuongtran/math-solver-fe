a0fb75fce92f06910fffbc0117f10a5219bcd9e3
Phan Hồ Phúc <phanhophuc12a1@gmail.com>
2021-07-16 19:34:16 +0700

    Update

--- src/components/define/defineStepOne.js
+++ src/components/define/defineStepOne.js
@@ -1,29 +1,39 @@
-import React from "react";
-import { Input } from "antd";
+import React, { useEffect } from "react";
+import { Input, Form } from "antd";
 import { Row, Col } from "antd";
-function defineStepOne(props) {
+const { TextArea } = Input;
+function DefineStepOne(props) {
     console.log(props);
-    props.state.data = "hihi";
     console.log(props);
+
+    useEffect(() => {
+        if (props.state.topic != undefined) {
+            document.getElementById("debai").value = props.state.topic;
+        }
+    });
+
+    const onChange = () => {
+        var debai = document.getElementById("debai").value;
+        props.setState("topic", debai);
+        console.log(props);
+    }
     return (
         <div style={{ height: "400px", paddingTop: "50px" }}>
-            <Row align="center">
-                Bước 2: Nhập thông tin đề bài
-            </Row>
-            <p>
-                <div>Nhập đề bài toán</div>
-                <Input
-                    style={{
-                        height: "200px"
-                    }}
-                    addonBefore="Đề bài"
-                    name="name"
-                    value={props.getState("name", "")}
-                    onChange={props.handleChange}
-                />
-            </p>
-        </div>
+            <Form onChange={onChange}>
+                <Form.Item
+
+                >
+                    <div>Tên bài toán</div>
+                    <TextArea id="debai"
+                        row={3}
+                    >
+
+                    </TextArea>
+                </Form.Item>
+            </Form>
+
+        </div >
     );
 }
 
-export default defineStepOne;
+export default DefineStepOne;
