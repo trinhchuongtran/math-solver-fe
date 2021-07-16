302026e232c8233912eba2fe244a02a0c2a5b238
Trình Trần <trinchuongtran@gmail.com>
2021-07-16 12:20:10 +0700

    trinh-update

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -4,9 +4,26 @@ import { Row, Col } from "antd";
 
 export default function FooterContent() {
   return (
-    <Row justify="space-between" style={{ backgroundColor: "white", height: "50px", position: "fixed", bottom: "0", width: "100%"}}>
-      <Col span={8} style={{margin: "auto 0", paddingLeft: "20px"}}>Ứng dụng hỗ trợ cho học sinh THCS giải bài tập</Col>
-      <Col span={4} style={{margin: "auto 0"}}>Copyright © 2021</Col>
+    <Row
+      style={{
+        backgroundColor: "white",
+        height: "50px",
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+      }}
+    >
+      <Row
+        justify="space-between"
+        style={{ padding: "0 150px", width: "100%" }}
+      >
+        <Col span={8} style={{ margin: "auto 0", paddingLeft: "20px" }}>
+          Ứng dụng hỗ trợ cho học sinh THCS giải bài tập
+        </Col>
+        <Col span={4} style={{ margin: "auto 0", textAlign: "end" }}>
+          Copyright © 2021
+        </Col>
+      </Row>
     </Row>
   );
 }
